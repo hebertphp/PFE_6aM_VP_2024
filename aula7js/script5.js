630e3e44fb7629048ceebd0273acae0e0d5105c9
Prof. Hebert <hebertphp@gmail.com>
2024-09-27 11:12:30 -0300

    Add files via upload

---
+++ aula7js/script5.js
@@ -0,0 +1,9 @@
+let a,b,c;
+let saida = document.getElementById("saida");
+a=10;
+b=10;
+c=11;
+saida.innerHTML="Menor = "+(a<b)+"<br>";//F
+saida.innerHTML+="Menor ou igual = "+(a<=b)+"<br>";//VT
+saida.innerHTML+="Maior = "+(c>b)+"<br>";//V
+saida.innerHTML+="Maior ou igual = "+(c>=b)+"<br>";//V
---
+++ aula7js/script9.js
@@ -0,0 +1,11 @@
+let a,b,c,d,e,f;
+let saida = document.getElementById("saida");
+a=50;
+b=120;
+c=200;
+d=!(a<=b) ? "logado" : "não logado";
+e=(a>=c) ? "blz" : "zoado";
+f=(a%2==0)?"PAR":"IMPAR";
+saida.innerHTML=`d = ${d}<br>`;
+saida.innerHTML+=`e = ${e}<br>`;
+saida.innerHTML+=`a = ${a} - é ${f}`;
